96d3a7f7b3e5120f85f6c5d470c0dc0f3d9f9e99
rajubhuiyan <rajubhuiyaann@gmail.com>
2022-01-10 14:59:06 +0600

    class 21 done

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import Navbar from '../Components/Navbar';
 
-const index = () => {
+export const getStaticProps = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await res.json();
+
+    return {
+        props: {
+            data
+        }
+    }
+}
+
+const index = ({ data }) => {
     return (
         <div>
-            <Navbar/>
+
+            <Navbar />
             <h1>this is blog</h1>
+
+
+            {
+            data.map((dt) =>
+                <div style={{margin:'40px', border: '1px solid red', padding: '20px'}} key={dt.id}>
+                    <h3>{dt.id}</h3>
+                    <h1>{dt.title}</h1>
+                </div>
+            )}
+
+
         </div>
     );
 };
