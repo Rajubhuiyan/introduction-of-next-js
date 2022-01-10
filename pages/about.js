792820c6f403f83cfacad87da4944661bd37e540
rajubhuiyan <rajubhuiyaann@gmail.com>
2022-01-10 14:10:46 +0600

    class 19 done

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,13 @@
+import Image from "next/image";
 import Navbar from "./Components/Navbar";
 
 const About = () => {
     return (
-        <div>
+        <div style={{textAlign: 'center'}}>
             <Navbar/>
             <h1>this is about</h1>
+            {/* <Image src="/tom.jpg" alt="" width="400" height="300"></Image> */}
+            <Image src="https://cdn.pixabay.com/photo/2020/05/11/15/38/tom-5158824_960_720.png" alt="" width="400" height="300"></Image>
         </div>
     );
 };
